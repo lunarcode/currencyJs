7d15f6ad9f1f6c968bab8efa1b51613a5adb68a6
Lunar {code} <33582536+lunarcode@users.noreply.github.com>
2018-02-06 22:49:16 -0600

    Create Plugin
    Se  crea la estructura para el plugin.

--- currency.js
+++ currency.js
@@ -1,33 +1,78 @@
-var divMoneda = document.getElementById('moneda');
-var txtMoneda = divMoneda.innerHTML;
+(function($){
 
-txtMoneda = txtMoneda.replace(/[^\d.-]/g, '');
+  $.fn.extend({
+    formatoMoneda : function(userConfig){
+             
+        var txtMoneda = '';
+        var valorOriginal = '';
+        var elMoneda = '';
+        var tipoTag = '';  
+        var txtFormatoMoneda = '$0.00';
 
-var partesMoneda = txtMoneda.split('.');
 
-if(partesMoneda[0].length > 0){
+        $(this).each(function() {
+            elMoneda = $(this);
+            tipoTag = elMoneda[0].tagName;  
 
-    var largoMoneda = partesMoneda[0].length;
-    var numeroComas = parseInt(largoMoneda/3);
-    var primerComa  = largoMoneda%3;
-    var numeroResultado = '';
-        
-    numeroResultado += partesMoneda[0].substr(0,primerComa);
+            if(tipoTag!='INPUT'){
 
-    for ( var pos = 0; pos < numeroComas ; pos++ ){
-        if( primerComa > 0 ){
-            numeroResultado += ',';
+                txtMoneda = $(this).html();
+                valorOriginal = $(this).html();
+                txtFormatoMoneda = generaFormatoMoneda();  
+                elMoneda.html(txtFormatoMoneda);
+
+            }
+
+            if(tipoTag==='INPUT'){
+
+                txtMoneda = $(this).val();
+                valorOriginal = $(this).val();
+                txtFormatoMoneda = generaFormatoMoneda();
+                elMoneda.val(txtFormatoMoneda);
+                
+            }
+
+        });
+
+
+        function generaFormatoMoneda(){
+            txtMoneda = txtMoneda.replace(/[^\d.-]/g, '');
+            var partesMoneda = txtMoneda.split('.');
+
+            if(partesMoneda[0].length > 0){
+
+                var largoMoneda = partesMoneda[0].length;
+                var numeroComas = parseInt(largoMoneda/3);
+                var primerComa  = largoMoneda%3;
+                var numeroResultado = '';
+
+                numeroResultado += partesMoneda[0].substr(0,primerComa);
+
+                for ( var pos = 0; pos < numeroComas ; pos++ ){
+                    if( primerComa > 0 ){
+                        numeroResultado += ',';
+                    }
+                    numeroResultado += partesMoneda[0].substr(primerComa,3);
+                    primerComa+=3;
+
+                }
+
+                if(partesMoneda[1] == null ||  partesMoneda[1] == ''){
+                     partesMoneda[1] = '00'; 
+                }
+
+                txtFormatoMoneda  = '$'+numeroResultado+'.'+partesMoneda[1];
+            }else{
+                txtFormatoMoneda = '$0.00';
+            }
+
+            return txtFormatoMoneda;
         }
-        numeroResultado += partesMoneda[0].substr(primerComa,3);
-        primerComa+=3;
+
+
 
     }
+  });
 
-    if(partesMoneda[1] == null ||  partesMoneda[1] == ''){
-         partesMoneda[1] = '00'; 
-    }
-    
-    console.log('$'+numeroResultado+'.'+partesMoneda[1]);
-}else{
-    console.log('$0.00');
-}
+
+})(jQuery)
